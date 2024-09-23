696534c73b6a7fffd0bc963a82cbf06b63029035
kai <kaim.kodehode@gmail.com>
2024-09-23 10:06:27 +0200

    first

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("Hello world!")
+
+let lives = 5;
+//lives = lives + 1
+//lives -= 3;
+
+lives ++
+
+// Operator
+//+ - * /
+
+//Variables
+//const let 
+
+//datatyper
+
+const string = "jørgen er kjempekul";
+console.log (string);
+
+const boolean = true; 
+
+console.log(boolean);
+
+const array = ["mandag", "tirsdag", "onsdag", "torsdag", "fredag", "lørdag", "søndag"]
+console.log (array);
