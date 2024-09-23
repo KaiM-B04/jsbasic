146f67a41eaceb8b3a2a5e93e4b8e576153fcc72
kai <kaim.kodehode@gmail.com>
2024-09-23 11:06:42 +0200

    added comments

--- script.js
+++ script.js
@@ -10,7 +10,13 @@ lives ++
 //+ - * /
 
 //Variables
-//const let 
+const konstant = 4
+//const er en kontant variabel som man ikke kan forandre innholdet på
+
+let variabel = 3
+//let er en variabel man kan forandre innholdet på
+variabel ++
+console.log(variabel);
 
 //datatyper
 
@@ -21,5 +27,16 @@ const boolean = true;
 
 console.log(boolean);
 
-const array = ["mandag", "tirsdag", "onsdag", "torsdag", "fredag", "lørdag", "søndag"]
-console.log (array);
+const array = [
+    "mandag", 
+    "tirsdag", 
+    "onsdag", 
+    "torsdag", 
+    "fredag", 
+    "lørdag", 
+    "søndag"
+];
+
+const textElements = "dette er en string";
+
+console.log (array[2], array[6]);
